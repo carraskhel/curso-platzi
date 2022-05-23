680735597a433bf05d8dd61c8bc3ee9f8788a17c
carraskhel <khelvyn95@gmail.com>
2022-05-23 14:11:53 -0400

    fin del curso profesional

--- curso_git.sql
+++ curso_git.sql
@@ -197,3 +197,15 @@ git grep khelvyn				: muestra los archivos que contienen khelvyn en todo el proy
 git grep -n khelvyn 			: ve que linea del archivo esta khelvyn
 
 git grep -c khelvyn 			: cuenta las veces se repite khelvyn por archivo
+
+
+--
+
+git shortlog -sn = muestra cuantos commit han hecho cada miembros del equipo.
+git shortlog -sn --all = muestra cuantos commit han hecho cada miembros del equipo hasta los que han sido eliminado
+git shortlog -sn --all --no-merge = muestra cuantos commit han hecho cada miembros quitando los eliminados sin los merges
+git blame ARCHIVO = muestra quien hizo cada cosa linea por linea
+git COMANDO --help = muestra como funciona el comando.
+git blame ARCHIVO -Llinea_inicial,linea_final= muestra quien hizo cada cosa linea por linea indicándole desde que linea ver ejemplo -L35,50
+git branch -a = se muestran todas las ramas tanto locales como remotas
+
